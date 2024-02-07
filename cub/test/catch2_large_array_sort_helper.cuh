21426cd7ec76d5c559dbafb1f6ee194212c8d102
Allison Vacanti <alliepiper16@gmail.com>
2024-02-07 22:32:41 +0000

    WIP start implementing key/value initialization.

--- cub/test/catch2_large_array_sort_helper.cuh
+++ cub/test/catch2_large_array_sort_helper.cuh
@@ -29,23 +29,29 @@
 
 #include <cub/util_type.cuh>
 
+#include <thrust/binary_search.h>
+#include <thrust/execution_policy.h>
+#include <thrust/fill.h>
 #include <thrust/memory.h>
 #include <thrust/random.h>
 #include <thrust/reverse.h>
 #include <thrust/shuffle.h>
 #include <thrust/tabulate.h>
 
+#include <cuda/std/iterator>
 #include <cuda/std/limits>
+#include <cuda/std/type_traits>
 
 #include <cstdint>
+#include <limits>
 
-#include "thrust/random.h"
 #include <c2h/cpu_timer.cuh>
 #include <c2h/device_policy.cuh>
 #include <c2h/vector.cuh>
 
 namespace detail
 {
+
 template <typename key_type>
 struct index_to_sorted_key
 {
@@ -59,6 +65,75 @@ struct index_to_sorted_key
     return static_cast<key_type>(idx * conversion);
   }
 };
+
+template <typename key_type>
+struct summary
+{
+  key_type key{};
+  std::size_t index{};
+  std::size_t count{};
+};
+
+template <typename key_type>
+struct index_to_summary
+{
+  using summary_t = summary<key_type>;
+
+  std::size_t num_items;
+  std::size_t num_summaries;
+
+  template <typename index_type>
+  _CCCL_HOST_DEVICE summary_t operator()(index_type idx) const
+  {
+    constexpr key_type max_key = ::cuda::std::numeric_limits<key_type>::max();
+
+    const double key_conversion = static_cast<double>(max_key) / static_cast<double>(num_summaries);
+    const key_type key          = static_cast<key_type>(idx * key_conversion);
+
+    const std::size_t elements_per_summary = num_items / num_summaries;
+    const std::size_t run_index            = idx * elements_per_summary;
+
+    const std::size_t run_size = idx == (num_summaries - 1) ? (num_items - run_index) : elements_per_summary;
+
+    return summary_t{key, run_index, run_size};
+  }
+};
+
+template <typename key_type, typename summary_iter_t>
+struct summary_to_sorted_keys
+{
+  using summary_t = typename ::cuda::std::iterator_traits<summary_iter_t>::value_type;
+
+  summary_iter_t summary_begin;
+  summary_iter_t summary_end;
+
+  struct binary_search_op
+  {
+    bool operator()(const summary_t& summary, std::size_t idx) const
+    {
+      return summary.index + summary.count < idx;
+    }
+  };
+
+  template <typename index_type>
+  key_type operator()(index_type idx) const
+  {
+    auto summary_iter =
+      thrust::lower_bound(thrust::seq, summary_begin, summary_end, static_cast<std::size_t>(idx), binary_search_op{});
+    return summary_iter->key;
+  }
+};
+
+template <typename value_type>
+struct index_to_value
+{
+  template <typename index_type>
+  value_type operator()(index_type index)
+  {
+    return static_cast<value_type>(index);
+  }
+};
+
 } // namespace detail
 
 template <typename key_type, typename value_type = cub::NullType>
@@ -104,5 +179,57 @@ struct large_array_sort_helper
     timer.print_elapsed_seconds_and_reset("Shuffle");
     keys_ref = keys_out;
     timer.print_elapsed_seconds_and_reset("D->H Copy Ref");
+    thrust::fill(c2h::device_policy, keys_out.begin(), keys_out.end(), key_type{});
+    timer.print_elapsed_seconds_and_reset("Reset Output");
+  }
+
+  void initialize_for_stable_key_value_sort(std::size_t num_items, bool is_descending)
+  {
+    static_assert(!::cuda::std::is_same<value_type, cub::NullType>::value, "value_type must be valid.");
+    using summary_t            = detail::summary<key_type>;
+    constexpr key_type max_key = ::cuda::std::numeric_limits<key_type>::max();
+
+    // Build the sorted reference arrays on the device in keys/values_out:
+    keys_out.resize(num_items);
+    values_out.resize(num_items);
+
+    const std::size_t max_summary_mem = num_items * (sizeof(key_type) + sizeof(value_type));
+    const std::size_t max_summaries   = cub::DivideAndRoundUp(max_summary_mem, sizeof(summary_t));
+    const std::size_t num_summaries   = std::min(max_summaries, num_items, static_cast<std::size_t>(max_key));
+
+    c2h::device_vector<summary_t> d_summaries;
+    // Overallocate -- if this fails, there won't be be enough free device memory for the input arrays.
+    // Better to fail now before spending time computing the inputs/outputs.
+    d_summaries.reserve(max_summaries);
+    d_summaries.resize(num_summaries);
+
+    // Populate the summaries using evenly spaced keys and constant sized runs, padding the last run to fill.
+    thrust::tabulate(c2h::device_policy,
+                     d_summaries.begin(),
+                     d_summaries.end(),
+                     detail::index_to_summary<key_type>{num_items, num_summaries});
+
+    // Use the summaries to populate the sorted keys
+    using summary_iterator_t = typename c2h::device_vector<summary_t>::const_iterator;
+    thrust::tabulate(
+      c2h::device_policy,
+      keys_out.begin(),
+      keys_out.end(),
+      detail::summary_to_sorted_keys<key_type, summary_iterator_t>{d_summaries.cbegin(), d_summaries.cend()});
+
+    // The sorted values will be the index truncated to the value type:
+    thrust::tabulate(c2h::device_policy, values_out.begin(), values_out.end(), detail::index_to_value<value_type>{});
+
+    // Copy the summaries to host memory and release device summary memory.
+    c2h::host_vector<summary_t> h_summaries = d_summaries;
+    d_summaries.clear();
+    d_summaries.shrink_to_fit();
+
+    // Build the unsorted key/value arrays on host in key/value_ref using Andy's algorithm.
+
+    // Release the host summary memory.
+    // Copy the unsorted data from key/value_ref -> key/value_in.
+    // Copy the sorted keys/values_out to host key/value_ref.
+    // Clear the output arrays to prevent no-op issues.
   }
 };
